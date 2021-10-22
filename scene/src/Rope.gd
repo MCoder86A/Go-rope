70527a2641fab2e30d3744440cbe927f7550d881
Mrinmoy Kalita <mrin443@gmail.com>
2021-10-22 15:37:58 +0530

    Changing approches

--- scene/src/Rope.gd
+++ scene/src/Rope.gd
@@ -1,9 +1,10 @@
 extends Line2D
 
-var rope_length: float = 10
+var rope_length: float = 60
 var direction: Vector2
 var is_rope_active: bool= true
 var speed: float = 10
+var nextPoint: Vector2 = Vector2.ZERO
 
 func _ready():
 	get_node("/root/Main/Ball").connect("update_state", self, "on_update_state")
@@ -12,13 +13,14 @@ func _ready():
 func _physics_process(delta):
 	var last_point: Vector2 = points[points.size()-1]
 	var lastSec_point: Vector2 = points[points.size()-2]
-	var nextPoint: Vector2 = last_point+lastSec_point.direction_to(last_point)*rope_length*delta
-	if lastSec_point.distance_to(nextPoint) >= rope_length :
-		add_point(nextPoint)
+	nextPoint += (lastSec_point.direction_to(last_point)*rope_length*delta)
+	if nextPoint.length() >= 10 :
+		add_point(last_point+nextPoint)
+		nextPoint.y = rope_length
+		position-=nextPoint*direction
+		nextPoint = Vector2(0,0)
 		remove_point(0)
 
 func on_update_state(new_direction: Vector2):
 	if is_rope_active:
 		direction = new_direction
-		position-=direction*rope_length*speed*get_physics_process_delta_time()
-		
