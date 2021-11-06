20ee2ad8dc24f697039a214e449d3711c11c231c
Mrinmoy Kalita <mrin443@gmail.com>
2021-11-06 13:50:02 +0530

    Changed approach
    Build using two point approach where each line has two points.
    TODO: start menu, gameover, score, texture design

--- scene/src/Ball.gd
+++ scene/src/Ball.gd
@@ -1,18 +1,31 @@
 extends KinematicBody2D
 
 signal Extend_rope(position)
-signal heading_toward()
+signal set_rope_state()
+signal on_destroy()
+
 # Import variable
 export var speed: float = 10
 export var path_p: NodePath
+export var timer_p: NodePath
+export var rope_width: float
 
 #Game state
 var direction: Vector2 = Vector2(1,0)
+var can_add_new_rope: bool = true
+var active_rope_direction: Vector2
+var active_rope_start_pt: Vector2
+var active_rope_end_pt: Vector2
+var pos_correction_fin: bool
+var swipe_start = null
+var minimum_drag = 50
 
 # Resources
 var path: Node2D
+var timer: Timer
 var res_Rope = load("res://scene/Rope.tscn")
-var ropes: Array
+
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 func drawLine(Rope: Line2D, firstPoint: Vector2, secondPoint: Vector2):
 	for i in firstPoint.distance_to(secondPoint) :
@@ -20,40 +33,149 @@ func drawLine(Rope: Line2D, firstPoint: Vector2, secondPoint: Vector2):
 
 func _ready():
 	path = get_node(path_p)
-	Rope_init()
+	timer = get_node(timer_p)
+	Rope_add(Vector2(position.x-50, position.y), direction)
+	rng.randomize()
+	timer.start(2)
 
-func _process(delta):
-	#Update_rope(delta)
-	pass
+func is_legal_move(new_move: Vector2):
+	var angle = round(direction.angle_to(new_move)*180/PI)
+	if angle == 90 or angle == -90:
+		return true
+	
+func move(new_move: Vector2):
+	if is_legal_move(new_move):
+		direction = new_move
 	
 func _physics_process(delta):
 	if Input.get_action_strength("wasd"):
-		direction.x = Input.get_action_strength("right")-Input.get_action_strength("left")
-		direction.y = Input.get_action_strength("down")-Input.get_action_strength("up")
+		var new_move: Vector2
+		new_move.x = Input.get_action_strength("right")-Input.get_action_strength("left")
+		new_move.y = Input.get_action_strength("down")-Input.get_action_strength("up")
+		move(new_move)
 	position+=direction*speed*delta
+	
 	emit_signal("Extend_rope", position)
 	
-func Rope_init():
-	var Rope: Line2D = res_Rope.instance()
+	if not pos_correction_fin:
+		correct_position()
+
+
+func on_rope_started():
+	correct_position()
+	if pos_correction_fin:
+		pos_correction_fin = false
+
+var target_pt: Vector2
+
+func correct_position():
+	target_pt = calculate_unmStart_pt()
+	if position.distance_to(target_pt) > 5:
+		var delta = get_physics_process_delta_time()
+		position +=  position.direction_to(target_pt)*speed/4*delta
+	else:
+		position = target_pt
+		pos_correction_fin = true
+
+func _on_BallTimer_timeout():
+	var rn = rng.randf_range(1,3)
+	if can_add_new_rope:
+		var unmodify_start_pt: Vector2 = calculate_unmStart_pt()+active_rope_direction*100
+		var new_direction = calculate_new_rope_dir()
+		var start_pt: Vector2 = calculate_rope_first_pt(unmodify_start_pt, new_direction)
+		Rope_add(start_pt, new_direction)
+	timer.start(rn)
+
+func calculate_new_rope_dir():
+	var rn_lr = rng.randi_range(0,1)
+	var new_direction: Vector2
+	if rn_lr:
+		new_direction = active_rope_direction.rotated(-(PI/2))
+	else:
+		new_direction = active_rope_direction.rotated(PI/2)
+
+	return new_direction
+	
+#return points intersected by perpendicular of Ball into line
+func calculate_unmStart_pt():
+	var new_x: float
+	var new_y: float
+	var p = position.x
+	var q = position.y
+	var x1 = active_rope_start_pt.x
+	var y1 = active_rope_start_pt.y
+	var x2 = active_rope_end_pt.x
+	var y2 = active_rope_end_pt.y
+	var m: float
+	if y2-y1 == 0:
+		new_y = y2
+		new_x = p
+	elif x2-x1 == 0:
+		new_x = x1
+		new_y = q
+	else:
+		m = (y2-y1)/(x2-x1)
+		new_x = ((p/m)+q+m*x1-y1)/(m+(1/m))
+		new_y = m*(new_x-x1)+y1
+		
+	return Vector2(new_x, new_y)
+	
+func calculate_rope_first_pt(start_pt: Vector2, new_direction: Vector2):
+	return start_pt+new_direction*rope_width/2
+	
+func Rope_add(start_pt: Vector2, to_direction: Vector2):
+	var Rope = res_Rope.instance()
+	var mat = Rope.get_material().duplicate()
+	Rope.set_material(mat)
+	
 	path.add_child(Rope)
-	ropes.append(Rope)
 	
-	for i in range(-200, 201, 400):
-		Rope.add_point(Vector2(position.x+i, position.y))
-	emit_signal("heading_toward")
+	Rope.add_point(start_pt)
+	Rope.add_point(start_pt+to_direction*200)
 	
-func Update_rope(delta: float):
-	for rope in ropes:
-		rope.position-=direction
+	active_rope_direction = to_direction
+	active_rope_start_pt = start_pt
+	active_rope_end_pt = start_pt+to_direction*200
 	
-	
-	
-	
-	
-	
-	
+	emit_signal("set_rope_state")
+	Rope.connect("being_destroy", self, "being_destroy")
+	Rope.connect("started", self, "on_rope_started")
+
+func being_destroy():
+	emit_signal("on_destroy")
 	
 	
-	
-	
-	
+func _on_B_RIGHT_button_up():
+	move(Vector2(1,0))
+
+func _on_B_LEFT_button_up():
+	move(Vector2(-1,0))
+
+func _on_B_DOWN_button_up():
+	move(Vector2(0,1))
+
+func _on_B_UP_button_up():
+	move(Vector2(0,-1))
+
+
+func _unhandled_input(event):
+	if event is InputEventScreenTouch:
+		if event.is_pressed():
+			swipe_start = event.position
+		if not event.is_pressed():
+			_calculate_swipe(event.position)
+		
+func _calculate_swipe(swipe_end):
+	if swipe_start == null: 
+		return
+	var swipe = swipe_end - swipe_start
+	if abs(swipe.x) > minimum_drag:
+		if swipe.x > 0:
+			move(Vector2(1,0))
+		else:
+			move(Vector2(-1,0))
+	if abs(swipe.y) > minimum_drag:
+		if swipe.y > 0:
+			move(Vector2(0,1))
+		else:
+			move(Vector2(0,-1))
--- scene/src/Rope.gd
+++ scene/src/Rope.gd
@@ -1,33 +1,136 @@
 extends Line2D
 
+signal being_destroy()
+signal started()
+
 var is_rope_active: bool= true
+var is_started: bool = false
 var delta_ball_position: Vector2 = Vector2.ZERO
 var last_ball_position: Vector2
 var heading_toward: Vector2
+var decaying_speed: float = 4
+var uvx: float = 0
+var Ball: KinematicBody2D
+var Ball_rect: Rect2
+var Rope_rect: Rect2
 
 func _ready():
-	get_node("/root/Main/Ball").connect("Extend_rope", self, "on_Extend_rope")
-	get_node("/root/Main/Ball").connect("heading_toward", self, "update_heading_toward")
+	Ball = get_node("/root/Main/Ball")
+	Ball.connect("Extend_rope", self, "on_Extend_rope")
+	Ball.connect("on_destroy", self, "on_rope_destroy")
+	Ball.connect("set_rope_state", self, "set_rope_state")
 	var ball: KinematicBody2D = get_node("/root/Main/Ball")
 	last_ball_position = ball.position
-	
-func update_heading_toward():
-	var last_second_point = points[points.size()-2]
-	var last_point = points[points.size()-1]
-	heading_toward = last_second_point.direction_to(last_point)
-	
+	set_ball_rope_rect()
+
+
 func _physics_process(delta):
 	pass
 
-func on_Extend_rope(new_position: Vector2):
-	delta_ball_position += new_position-last_ball_position
-	last_ball_position = new_position
-	var last_second_point = points[points.size()-2]
+func set_ball_rope_rect():
+	var Ball_texture: Sprite = Ball.get_node("Ball")
+	var width = Ball_texture.texture.get_width()
+	var height = Ball_texture.texture.get_height()
+	var ball_position = Vector2(Ball.position.x-width/2, Ball.position.y-height/2)
+	Ball_rect = Rect2(Vector2(ball_position), Vector2(width, height))
+
+func get_rope_size():
+	var first_point: Vector2 = points[0]
+	var last_point: Vector2 = points[points.size()-1]
+	var rope_height: float = first_point.distance_to(last_point)
+	var x: float
+	var y: float
+	if heading_toward.x == 1 or heading_toward.x == -1:
+		x = rope_height
+		y = width
+	elif heading_toward.y == 1 or heading_toward.y == -1:
+		x = width
+		y = rope_height
+	
+	return Vector2(x, y)
+
+func get_rope_position():
+	var first_point: Vector2 = points[0]
+	var last_point: Vector2 = points[points.size()-1]
+	var x: float
+	var y: float
+	if heading_toward.x == 1:
+		x = first_point.x
+		y = first_point.y-(width/2)
+	elif heading_toward.x == -1:
+		x = last_point.x
+		y = last_point.y-(width/2)
+	elif heading_toward.y == 1:
+		x = first_point.x-(width/2)
+		y = first_point.y
+	elif heading_toward.y == -1:
+		x = last_point.x-(width/2)
+		y = last_point.y
+	
+	return Vector2(x, y)
+
+func update_ball_rope_rect():
+	var ball_x = Ball.position.x-Ball_rect.size.x/2
+	var ball_y = Ball.position.y-Ball_rect.size.y/2
+	var ball_position = Vector2(ball_x, ball_y)
+	Ball_rect = Rect2(Vector2(ball_position), Ball_rect.size)
+	
+	var rope_position = get_rope_position()
+	var rope_size = get_rope_size()
+	Rope_rect = Rect2(rope_position, rope_size)
+
+#Called when if any rope get inactivated
+func on_rope_destroy():
+	if Ball_rect.intersects(Rope_rect) and not is_started:
+		is_started = true
+		emit_signal("started")
+		
+
+func set_rope_state():
+	var first_point = points[0]
 	var last_point = points[points.size()-1]
-	if delta_ball_position.length()>=5:
-		add_point(last_point+heading_toward*delta_ball_position)
+	heading_toward = first_point.direction_to(last_point)
+	if get_node("/root/Main/Path").get_child_count() == 1:
+		is_started = true
+	is_rope_active = true
+	
+func on_Extend_rope(new_position: Vector2):
+	update_ball_rope_rect()
+	if not Ball_rect.intersects(Rope_rect) and is_started == true and is_rope_active == true:
+		is_rope_active = false
+		emit_signal("being_destroy")
+		$fire_point1.emitting = true
+		$fire_point2.emitting = true
+	
+	delta_ball_position += new_position-last_ball_position
+	delta_ball_position = Vector2(abs(delta_ball_position.x), abs(delta_ball_position.y))
+	last_ball_position = new_position
+	var first_point: Vector2 = points[0]
+	var last_point: Vector2 = points[points.size()-1]
+	if delta_ball_position.length()>=1 and is_rope_active and is_started:
+		var first_pt = first_point+heading_toward*delta_ball_position*0
+		var second_pt = last_point+heading_toward*delta_ball_position
+		set_rope_position(first_pt, second_pt)
 		delta_ball_position = Vector2.ZERO
-		remove_point(0)
+	elif not is_rope_active and is_started:
+		var first_pt = first_point+heading_toward*decaying_speed
+		var second_pt = last_point-heading_toward*decaying_speed
+		set_rope_position(first_pt, second_pt)
+		if first_point.distance_to(last_point)<width:
+			queue_free()
+
+func set_rope_position(first_pt_vec: Vector2, second_pt_vec: Vector2):
+	var first_point: Vector2 = points[0]
+	set_point_position(0, first_pt_vec)
+	set_point_position(1, second_pt_vec)
+	$fire_point1.position = first_point
+	$fire_point1.direction = -heading_toward
+	$fire_point2.position = second_pt_vec
+	$fire_point2.direction = heading_toward
+	var delta_first_point: Vector2 = points[0]-first_point
+	uvx += fmod(delta_first_point.length()/width, 1)
+	material.set_shader_param("x_axis", uvx)
+
 
 
 
